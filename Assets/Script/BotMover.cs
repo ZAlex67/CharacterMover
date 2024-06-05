1b8b76e3d6fa7fe78d94102ad085d4b19c24352f
Aleksandr <zabrodinalex67@gmail.com>
2024-06-05 14:12:57 +0900

    Fix bot

--- Assets/Script/BotMover.cs
+++ Assets/Script/BotMover.cs
@@ -6,27 +6,31 @@ public class BotMover : MonoBehaviour
     [SerializeField] private Player _player;
     [SerializeField] private float _moveSpeed = 3f;
     [SerializeField] private float _stopDistance = 2f;
-    [SerializeField] private float _stepHeight = 0.5f;
+    [SerializeField] private float _botHeight;
     [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private float _maxForce;
+    [SerializeField] private float _groundDrag;
 
     private Rigidbody _rigidbody;
-    private bool _isGrounded;
+    private bool _isGround;
 
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        SearchGroundUnderFeet();
+    }
+
     private void FixedUpdate()
     {
-        CheckGround();
+        Move();
+    }
 
-        if (!_isGrounded)
-        {
-            _rigidbody.velocity += Physics.gravity * Time.fixedDeltaTime;
-            return;
-        }
-
+    private void Move()
+    {
         float distanceToTarget = Vector3.Distance(transform.position, _player.transform.position);
 
         if (distanceToTarget <= _stopDistance)
@@ -35,28 +39,34 @@ public class BotMover : MonoBehaviour
             return;
         }
 
-        Vector3 direction = (_player.transform.position - transform.position).normalized;
-        float line = 0.5f;
-        float radius = 1f;
-        float stair = 0.1f;
+        Vector3 currentVelocity = _rigidbody.velocity;
+        Vector3 targetVelocity = (_player.transform.position - transform.position).normalized;
+        targetVelocity *= _moveSpeed;
 
-        Vector3 cast = transform.position + Vector3.up * stair;
-        if (Physics.SphereCast(cast, radius, direction, out RaycastHit hit, line))
-        {
-            Vector3 stepCheck = transform.position + direction * line;
-            if (!Physics.Raycast(stepCheck + Vector3.up * (_stepHeight + stair), Vector3.down, out RaycastHit stepHit, _stepHeight, _groundMask))
-            {
-                _rigidbody.velocity = direction + Vector3.up * _stepHeight * Time.fixedDeltaTime - Physics.gravity;
-            }
-        }
+        targetVelocity = transform.TransformDirection(targetVelocity);
 
-        Vector3 move = direction * _moveSpeed * Time.fixedDeltaTime;
-        _rigidbody.MovePosition(_rigidbody.position + move);
+        Vector3 velocityChange = targetVelocity - currentVelocity;
+        velocityChange = new Vector3(velocityChange.x, 0, velocityChange.z);
+
+        Vector3.ClampMagnitude(velocityChange, _maxForce);
+
+        _rigidbody.AddForce(velocityChange, ForceMode.Impulse);
     }
 
-    private void CheckGround()
+    private void SearchGroundUnderFeet()
     {
-        float distance = 1.1f;
-        _isGrounded = Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, distance, _groundMask);
+        float feetLength = 0.5f;
+        float ground = 0.2f;
+
+        _isGround = Physics.Raycast(transform.position, Vector3.down, _botHeight * feetLength + ground, _groundMask);
+
+        if (_isGround)
+        {
+            _rigidbody.drag = _groundDrag;
+        }
+        else
+        {
+            _rigidbody.drag = 0;
+        }
     }
 }
